fd33ddb6cc90bf65cbe3247bb8e479868db3e3dc
vanshika-wq <juhisingh7182018@gmail.com>
2025-10-18 20:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (11.41%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
