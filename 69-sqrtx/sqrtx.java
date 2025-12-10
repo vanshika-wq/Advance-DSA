f4454f4050105f4b2323e6c6ab28b73868c351c5
vanshika-wq <juhisingh7182018@gmail.com>
2025-12-10 22:29:47 +0530

    Time: 1 ms (93.37%) | Memory: 42.5 MB (32.72%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        long low = 1, high= x/2+1;
+        while (low < high ){
+            long mid = low + (high - low) /2;
+            if ( mid > x/ mid ){
+                high = mid;
+            } else {
+                low = mid +1;
+
+            }
+        }
+        return (int ) (low - 1);
+        
+    }
+}
