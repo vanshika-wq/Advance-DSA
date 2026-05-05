22bf23fcbae489355ecbb4590fd4c2b5d39f45ee
vanshika-wq <juhisingh7182018@gmail.com>
2026-05-05 13:32:54 +0530

    Time: 2 ms (99.85%) | Memory: 43.6 MB (52.99%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -3,18 +3,18 @@ class Solution {
         int m = num1.length(), n = num2.length();
         int[] res = new int[m + n];
         
-        // Fill partial products (like manual multiplication)
+        
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
                 int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
                 int p1 = i + j, p2 = i + j + 1;
                 int sum = mul + res[p2];
                 res[p2] = sum % 10;
-                res[p1] += sum / 10;  // Carry to next position
+                res[p1] += sum / 10;  
             }
         }
         
-        // Handle remaining carries
+        
         for (int i = m + n - 1; i > 0; i--) {
             if (res[i] >= 10) {
                 res[i-1] += res[i] / 10;
@@ -22,7 +22,7 @@ class Solution {
             }
         }
         
-        // Build string, skip leading zeros
+        
         StringBuilder sb = new StringBuilder();
         int start = 0;
         while (start < m + n - 1 && res[start] == 0) start++;
