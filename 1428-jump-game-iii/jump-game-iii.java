f4a0bacde3cc0406a5d5b105ecbafdc61933eca0
vanshika-wq <juhisingh7182018@gmail.com>
2026-05-17 22:05:19 +0530

    Time: 1 ms (99.73%) | Memory: 52.4 MB (40.50%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if (start < 0 || start >= arr.length || arr[start] < 0) {
+            return false;
+        }
+        
+        if (arr[start] == 0) {
+            return true;
+        }
+        
+        int jumpLength = arr[start];
+        arr[start] = -arr[start]; 
+        
+        return canReach(arr, start + jumpLength) || canReach(arr, start - jumpLength);
+    }
+}
